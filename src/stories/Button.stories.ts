cd29350de3e38d071351943b056a13cd02e196d0
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-07-20 06:54:54 +0700

    feat: add definition story

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -1,7 +1,7 @@
 //configure Meta data dengan ekspor default
 
 //import Metadata dari Storybook
-import type { Meta } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/react";
 
 //import Button dari Component Button
 import { Button } from "../Components/Button";
@@ -11,5 +11,13 @@ const meta: Meta<typeof Button> = {
    component: Button,
 };
 
+//export meta
 export default meta;
 //end configure Meta data
+
+//mendefinisikan cerita
+type Story = StoryObj<typeof Button>;
+
+export const Primary: Story = {
+   render: () => <Button primary label='Button' />,
+};
