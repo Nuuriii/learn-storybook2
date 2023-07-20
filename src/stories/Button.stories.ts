9f0f36ab95f6e11d5f4fd9aefc6e65ad61d783eb
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-07-20 14:04:45 +0700

    feat: add comment for understand storybook

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -1,3 +1,4 @@
+//import Metadata dan type dari storybook yang digunakan untuk mendefinisikan cerita dalam bentuk objek
 import type { Meta, StoryObj } from "@storybook/react";
 
 import { Button } from "../Components/Button";
