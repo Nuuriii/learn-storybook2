2ef84b86886486e8e6d7d6d3892141eaffc1e15d
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-07-20 16:29:22 +0700

    feat: finishing component paragraph

---
+++ src/Components/Paragraph.tsx
@@ -0,0 +1,24 @@
+interface ParagraphProps {
+   primary?: boolean;
+   color?: string;
+   size?: "small" | "medium" | "large";
+   label?: string;
+}
+
+export const Paragraph = ({
+   primary = false,
+   color,
+   size = "medium",
+   label,
+}: ParagraphProps) => {
+   const mode = primary ? "sb-paragraph--primary" : "sb-paragraph--secondary";
+
+   return (
+      <p
+         className={["sb-paragraph", `sb-paragraph--${size}`, mode].join(" ")}
+         style={{ color }}
+      >
+         {label}
+      </p>
+   );
+};
