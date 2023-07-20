981f13fa2543096e08615fb8c84d39285cebcf28
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-07-20 17:02:10 +0700

    feat: change to react

---
+++ src/stories/Paragraph.stories.ts
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Paragraph } from "../Components/Paragraph";
+
+//konfigurasi story
+const meta: Meta<typeof Paragraph> = {
+   title: "Example/Paragraph",
+   component: Paragraph,
+   tags: ["autodocs"],
+   argTypes: {
+      color: { control: "color" },
+   },
+};
+
+export default meta;
+type Story = StoryObj<typeof Paragraph>;
