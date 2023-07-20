96dc7c854ae8a02f5fcda9d43d54fb325e796126
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-07-20 13:13:28 +0700

    feat: remake component & story

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -1,46 +1,4 @@
-//import file css untuk styling
+import React from "react";
 import "../css/button.css";
 
-//definisikan tipe properti yang dapat diterima Component Button
-interface ButtonProps {
-   //properti untuk
-   primary?: boolean;
-   //properti yang menyimpan warna background apa yang dipakai
-   backgroundColor?: string;
-   //properti untuk mengatur ukuran, hanya terdapat 3 pilihan
-   size?: "small" | "medium" | "large";
-   //properti untuk mengatur nama tombol
-   label: string;
-   //properti untuk mengatur event ketika tombol diklik
-   onClick?: () => void;
-}
-
-/**
- * Primary UI component for user interaction
- */
-export const Button = ({
-   primary = false,
-   size = "medium",
-   backgroundColor,
-   label,
-   ...props
-}: ButtonProps) => {
-   //variable yang digunakan untuk menentukan Class css apa yang akan digunakan berdasarkan property primary
-   const mode = primary
-      ? "storybook-button--primary"
-      : "storybook-button--secondary";
-   return (
-      <button
-         type='button'
-         className={[
-            "storybook-button",
-            `storybook-button--${size}`,
-            mode,
-         ].join(" ")}
-         style={{ backgroundColor }}
-         {...props}
-      >
-         {label}
-      </button>
-   );
-};
+//buat properti untuk mengatur props apa saja yang dapat digunakan oleh component Button
