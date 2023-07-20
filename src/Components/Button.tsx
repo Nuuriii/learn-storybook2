c2863046ed23771a89cef063ff9f8f1817e853a6
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-07-20 13:17:32 +0700

    feat: add interface to set property for props

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -2,3 +2,11 @@ import React from "react";
 import "../css/button.css";
 
 //buat properti untuk mengatur props apa saja yang dapat digunakan oleh component Button
+interface ButtonProps {
+   //properti primary yang bertipe data boolean untuk mengecek apakah primary benar atau salah
+   primary?: boolean;
+   //properti untuk mengatur warna background yang digunakan
+   backgroundColor?: string;
+   //properti untuk mengatur ukuran button
+   size?: "small" | "medium" | "large";
+}
