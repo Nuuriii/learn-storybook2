ea8fbc1e94e967f7c631205a934e7d6dad630d96
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-07-21 14:07:06 +0700

    feat: add Header Component

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 import { Button } from "./Button";
 
 type User = {
@@ -48,10 +46,20 @@ export const Header = ({
          <div>
             {user ? (
                <>
-               <span className="welcome">
-                  Welcome, <b>{user.name}</b>!
-               </span>
-               <Button size="small" onClick={onLogout} label="Log out" />
+                  <span className='welcome'>
+                     Welcome, <b>{user.name}</b>!
+                  </span>
+                  <Button size='small' onClick={onLogout} label='Log out' />
+               </>
+            ) : (
+               <>
+                  <Button size='small' onClick={onLogin} label='Log in' />
+                  <Button
+                     primary
+                     size='small'
+                     onClick={onCreateAccount}
+                     label='Sign up'
+                  />
                </>
             )}
          </div>
