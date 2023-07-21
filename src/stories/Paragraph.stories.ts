ee60b566a53b2192238d6eeae807fd019dca9c9e
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-07-21 10:39:25 +0700

    feat: add story for component

--- src/stories/Paragraph.stories.ts
+++ src/stories/Paragraph.stories.ts
@@ -18,6 +18,27 @@ type Story = StoryObj<typeof Paragraph>;
 export const Primary: Story = {
    args: {
       primary: true,
-      label: "Button",
+      label: "Ini Text",
+   },
+};
+
+export const Secondary: Story = {
+   args: {
+      size: "large",
+      label: "ukuran besar dengan tema secondary",
+   },
+};
+
+export const Large: Story = {
+   args: {
+      size: "large",
+      label: "Ini memiliki ukuran yang sangat besar",
+   },
+};
+
+export const Small: Story = {
+   args: {
+      size: "small",
+      label: "Ini Berukuran small yaa..",
    },
 };
