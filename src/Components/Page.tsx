419e3ed09adcbc9539bc94a73b8a6ebd1bdbb06b
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-07-21 15:22:12 +0700

    feat: add content in Page.tsx

--- src/Components/Page.tsx
+++ src/Components/Page.tsx
@@ -17,6 +17,21 @@ export const Page = () => {
             onLogout={() => setUser(undefined)}
             onCreateAccount={() => setUser({ name: "Nurrohman" })}
          />
+
+         <section className='storybook-page'>
+            <h2>Pages in Storybook</h2>
+            <p>
+               We recommend building UIs with a
+               <a
+                  href='https://componentdriven.org'
+                  target='_blank'
+                  rel='noopener noreferrer'
+               >
+                  <strong>component-driven</strong>
+               </a>
+               process starting with atomic components and ending with pages.
+            </p>
+         </section>
       </article>
    );
 };
