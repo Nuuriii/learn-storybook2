3cd6f92a57103a5f1dcb9e0260cbc0bdcb7501be
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-07-21 14:17:31 +0700

    feat: add story for component

--- src/stories/Header.stories.ts
+++ src/stories/Header.stories.ts
@@ -1,3 +1,28 @@
+//import metadata dari storybook
 import type { Meta, StoryObj } from "@storybook/react";
 
 import { Header } from "../Components/Header";
+
+//Konfigurasi Story
+const meta = {
+   title: "Example/Header",
+   component: Header,
+   tags: ["autodocs"],
+   parameters: {
+      layout: "fullscreen",
+   },
+} satisfies Meta<typeof Header>;
+
+//export variable meta
+export default meta;
+type Story = StoryObj<typeof Header>;
+
+export const LoggedIn: Story = {
+   args: {
+      user: {
+         name: "Nurrohman",
+      },
+   },
+};
+
+export const LoggedOut: Story = {};
