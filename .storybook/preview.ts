2c95eabbd7bb5fe042004fd12732c75373dfe23b
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-07-22 12:55:31 +0700

    feat: add styled component

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,15 +1,25 @@
 import type { Preview } from "@storybook/react";
 
+// .storybook/preview.js
+import { withThemeFromJSXProvider } from "@storybook/addon-styling";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    font-family: "Nunito Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
+  }
+`;
+
 const preview: Preview = {
-  parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/,
+   parameters: {
+      actions: { argTypesRegex: "^on[A-Z].*" },
+      controls: {
+         matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/,
+         },
       },
-    },
-  },
+   },
 };
 
 export default preview;
